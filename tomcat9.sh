b166acee5584da3b865ddbf20ed39b60315549c0
prasadpvv001 <146353576+prasadpvv001@users.noreply.github.com>
2023-10-30 22:28:44 +0530

    Update tomcat9.sh

--- tomcat9.sh
+++ tomcat9.sh
@@ -4,8 +4,8 @@ sudo apt install tomcat9 tomcat9-admin -y
 sudo ufw allow from any to any port 9090 proto tcp
 rm -rf /etc/tomcat9/server.xml
 rm -rf /etc/tomcat9/tomcat-users.xml
-sudo cp /home/ubuntu/tomct/server.xml /etc/tomcat9/
-sudo cp /home/ubuntu/tomct/tomcat-users.xml /etc/tomcat9/
+sudo cp /home/ubuntu/tomansi/server.xml /etc/tomcat9/
+sudo cp /home/ubuntu/tomansi/tomcat-users.xml /etc/tomcat9/
 #sudo vim /etc/tomcat9/server.xml
 	// Change the port number from 8080 to 9090
 #sudo vim /etc/tomcat9/tomcat-users.xml
